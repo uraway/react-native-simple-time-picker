d62b615ef40c075b12222debc3182330fa9a88c3
uraway <masato.uraway@gmail.com>
2017-08-31 13:41:51 +0800

    init

---
+++ sample/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import TimePicker from './main';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default class App extends Component {
+  state = {
+    selectedHours: 0,
+    selectedMinutes: 0,
+  }
+
+  render() {
+    const { selectedHours, selectedMinutes } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text>{selectedHours}:{selectedMinutes}</Text>
+        <TimePicker
+          selectedHours={selectedHours}
+          selectedMinutes={selectedMinutes}
+          onChange={(hours, minutes) => this.setState({ selectedHours: hours, selectedMinutes: minutes })}
+        />
+      </View>
+    );
+  }
+}
